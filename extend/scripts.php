675cb92299dd932009ca88816bd5194ba9df0691
george39 <george35@utp.edu.co>
2018-03-18 09:47:21 -0500

    paso 1.1 creacion de los scripts

--- extend/scripts.php
+++ extend/scripts.php
@@ -1,6 +1,6 @@
-<!--1.1  -->
+<!--1.1  material traido de jquery y materialize-->
 </main>
-<script
+<script 
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
