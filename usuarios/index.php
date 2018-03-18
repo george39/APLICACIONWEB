d78e432645990a71dcc4fc5fce01d6fb644bf1dd
george39 <george35@utp.edu.co>
2018-03-18 14:26:23 -0500

    codigo del metodo ajax para validar disponibilidad de usuarios

--- usuarios/index.php
+++ usuarios/index.php
@@ -12,8 +12,13 @@
 				<form class="form" action="ins_usuarios.php" method="post" enctype="multipart/form-data">
 					<div class="input-field">
 				<!-- onblur="may(this.value, this.id"> es para dejar enviar todos los datos del formulario en mayusculas -->
-						<input type="text" name="nick" required autofocus title="DEBE DE TENER ENTRE 8 Y 15 CARACTERES, SOLO LETRAS" pattern="[A-Za-z]{8,15}" id="nick" onblur="may(this.value, this.id)">	
-					</div>
+						<input type="text" name="nick" required autofocus title="DEBE DE TENER ENTRE 8 Y 15 CARACTERES, SOLO LETRAS" pattern="[A-Za-z]{8,15}" id="nick" onblur="may(this.value, this.id)">
+						<!--2.4 -->
+						<label for="nick">Nick</label>	
+						</div>
+
+					<!--2.3 validar si el usuario existe en la bd -->
+					<div class="validacion"></div>
 				</form>
 			</div>
 		</div>
@@ -21,6 +26,22 @@
 </div>
 
 <?php include '../extend/scripts.php'; ?>
+
+<!--2.3 validar que el usuario existe en la bd con metodo ajax -->
+<script>
+	$('#nick').change(function() {
+		$.post('ajax_validacion_nick.php',{
+			nick:$('#nick').val(),
+
+			beforeSend: function(){
+				$('validacion').html("Espere un momento por favor");
+			}
+
+		}, function(respuesta){
+			$('.validacion').html(respuesta);
+		});
+	});
+</script>
 
 </body>
 </html>
