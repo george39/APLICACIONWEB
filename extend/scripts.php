6fc1681bb7331d12812f0d64195138937eff7fac
george39 <george35@utp.edu.co>
2018-03-18 20:39:41 -0500

    3.2 mostrar datos en pantalla y barra de busqueda

--- extend/scripts.php
+++ extend/scripts.php
@@ -9,6 +9,18 @@
 <script src="../cdn/sweetalert2.js"></script>
 
 <script>
+
+/* 3.2 script para llamar al input buscar y filtrar cuando se valla escribiendo */
+	$('#buscar').keyup(function(event) {
+		var contenido = new RegExp($(this).val(), 'i'); /* este codigo es para que sea sencible a mayusculas y minusculas*/
+		$('tr').hide(); /* para ocultar las tr*/
+		$('tr').filter(function() {  /* para hacer el filtro dentro de las tr que ya ha oculatado*/
+			return contenido.test($(this).text());
+		}).show(); /* para que muestre lo que filtra*/ 
+		$('.cabecera').attr('style',''); /* para que no me filtre las cabeceras y me las muestre*/
+		
+	});
+
 	$('.button-collpase').sideNav();/*1.5 para que funcione el menu en celulares */
 	$('select').material_select();  /*2.5 inicializa el select de seleccionar el nivel de usuarios se vea */
 
