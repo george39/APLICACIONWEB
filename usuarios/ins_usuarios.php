e7c3122d19bdc9dc0b25d7ea236ce1e4537cc03f
george39 <george35@utp.edu.co>
2018-03-18 19:41:40 -0500

    2.9 validaciones del formulario pero con php

--- usuarios/ins_usuarios.php
+++ usuarios/ins_usuarios.php
@@ -12,6 +12,54 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$nivel = $con->real_escape_string(htmlentities($_POST['nivel']));
 	$nombre = $con->real_escape_string(htmlentities($_POST['nombre']));
 	$correo = $con->real_escape_string(htmlentities($_POST['correo']));
+
+	#2.9
+	#validaion del formulario con php 
+	if (empty($nick) || empty($pas1) || empty($nombre)) {
+		header('location:../extend/alerta.php?msj=Hay un campo sin especificar&c=us&p=in&t=error');
+		exit;
+	}
+
+	if (!ctype_alpha($nick)) {
+		header('location:../extend/alerta.php?msj=El nick no contiene solo letras&c=us&p=in&t=error');
+		exit;
+	}
+
+	if (!ctype_alpha($nivel)) {
+		header('location:../extend/alerta.php?msj=El nivel no contiene solo letras&c=us&p=in&t=error');
+		exit;
+	}
+
+	#para validar el nombre y permitirle espacios 
+	$caracteres = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ ";
+	for ($i=0; $i < strlen($nombre); $i++) { #strlen me trae la cantidad de carecteres que tiene el campo nombre 
+		$buscar = substr($nombre,$i,1); #substr sustrae de la posision i un caracter
+		if (strpos($caracteres,$buscar) ===false) { # la funcion strpos busca en el array caracteres la variable buscar
+			header('location:../extend/alerta.php?msj=El nombre no contiene solo letras&c=us&p=in&t=error');
+		exit;
+		}
+	}
+
+	$usuario = strlen($nick);
+	$contrasenia = strlen($pas1);
+
+	if ($usuario < 8 || $usuario >15) {
+		header('location:../extend/alerta.php?msj=El nick debe contener entre 8 y 15 caracteres&c=us&p=in&t=error');
+		exit;
+	}
+
+	if ($contrasenia < 8 || $contrasenia >15) {
+		header('location:../extend/alerta.php?msj=la contraseña debe contener entre 8 y 15 caracteres&c=us&p=in&t=error');
+		exit;
+	}
+
+	if (!empty($correo)) {
+		if (!filter_var($correo,FILTER_VALIDATE_EMAIL)) {
+			header('location:../extend/alerta.php?msj=El email no es valido&c=us&p=in&t=error');
+		exit;
+		}
+	}
+
 }else {   
 	header('location:../extend/alerta.php?msj=Utiliza el formulario&c=us&p=in&t=error');
 
